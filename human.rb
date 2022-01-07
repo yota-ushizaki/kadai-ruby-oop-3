a200d389db6b316f1d00db720619c644b7a3e03a
yota.ushizaki <nutsuray.027@gmail.com>
2022-01-07 15:36:37 +0000

    Lesson 9

---
+++ human.rb
@@ -0,0 +1,11 @@
+require './animal'
+require './thinkable'
+class Human < Animal
+    attr_accessor :hobby
+    
+    def initialize(name, age, hobby)
+        self.name = name
+        self.age = age
+        self.hobby = hobby
+    end
+end
---
+++ main.rb
@@ -0,0 +1,15 @@
+require './animal'
+require './thinkable'
+require './human'
+include Thinkable
+
+tanaka = Human.new("田中太郎", 25, "電車")
+suzuki = Human.new("鈴木次郎", 30, "野球")
+sato   = Human.new("佐藤花子", 20, "映画")
+
+tanaka.say
+tanaka.think
+suzuki.say
+suzuki.think
+sato.say
+sato.think
